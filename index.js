2ce96a43e112414622854856d93a35b90c886d5b
EventReplay <49799927+EventReplay@users.noreply.github.com>
2019-04-22 03:51:53 +0100

    Update Login, add recall preset, bespoke command

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ instance.prototype.incomingData = function(data) {
 	debug(data);
 
 	// Match part of the copyright response from unit when a connection is made.
-	// Then send Info request which should reply with Matrix setup, eg: "V8X4 A8X4"
+	// Send Info request which should reply with Matrix setup, eg: "V8X4 A8X4"
 	if (self.login === false && data.match("Extron Electronics DXP")) {
 		self.status(self.STATUS_WARNING,'Logging in');
 		self.socket.write("I"+ "\n");
